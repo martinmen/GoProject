11c73e64f8cb7f1204fa290c47e43d097a6ab389
martinmen <martinhmsuni@gmail.com>
2020-12-23 11:33:52 -0300

    add Middleware
    +basic authentication

--- server.go
+++ server.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 	"gitlab.com/pragmaticreviews/golang-gin-poc/controller"
+	"gitlab.com/pragmaticreviews/golang-gin-poc/middlewares"
 	"gitlab.com/pragmaticreviews/golang-gin-poc/service"
 )
 
@@ -11,8 +16,15 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutPut() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+	setupLogOutPut()
+	server := gin.New()
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/videos", func(context *gin.Context) {
 		context.JSON(200, videoController.FindAll())
